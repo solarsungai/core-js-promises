76e894d3d54f8b9748ecef7acce4b3cd3fbf1458
solarsungai <solarsungai@gmail.com>
2026-02-02 21:40:27 +0200

    feat: 7 passing

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -142,8 +142,14 @@ async function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce((chain, currentPromise) => {
+    return chain.then((accumulatedString) => {
+      return currentPromise.then((value) => {
+        return accumulatedString + value;
+      });
+    });
+  }, Promise.resolve(''));
 }
 
 module.exports = {
